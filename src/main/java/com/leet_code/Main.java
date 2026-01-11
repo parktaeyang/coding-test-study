3049d4437899184880f89db57afcaa062da5ed11
박태양 <parkxodid1234@gmail.com>
2026-01-11 12:39:01 +0900

    initial commit

---
+++ src/main/java/com/leet_code/Main.java
@@ -0,0 +1,10 @@
+package com.leet_code;
+
+public class Main {
+    public static void main(String[] args) {
+        P1_Two_Sum p1TwoSum = new P1_Two_Sum();
+
+        int[] result = p1TwoSum.testExample();
+        System.out.println(result);
+    }
+}
